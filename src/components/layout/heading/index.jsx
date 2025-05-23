72464e2a4d30498442149cea6298aa174683ab16
John Brown <jb20061027@gmail.com>
2025-05-23 03:21:43 -0500

    fixed/some-styles

--- src/components/layout/heading/index.jsx
+++ src/components/layout/heading/index.jsx
@@ -5,7 +5,7 @@ import { datas } from '../../data';
 export const Heading = ({ type }) => {
     return (
         <div className="w-full heading">
-            <div className='flex flex-col pt-[222px] pb-[120px] px-6 w-fit mx-auto'>
+            <div className='flex flex-col sm:pt-[222px] pt-[180px] sm:pb-[120px] pb-[80px] px-6 w-fit mx-auto'>
                 {datas.heading[type - 1].title}
                 <p className='text-wrap'>{datas.heading[type - 1].content}</p>
             </div>
